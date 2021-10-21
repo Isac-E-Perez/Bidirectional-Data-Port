3b83317145f716c1c6303f3eae1fe12be670e7f2
Isac E Perez <89553126+Isac-E-Perez@users.noreply.github.com>
2021-10-21 13:53:16 -0500

    Update biport_tb.vhdl

--- biport_tb.vhdl
+++ biport_tb.vhdl
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity biport_tb is 
 end biport_tb;
   
-architecture behavioral for biport_tb is
+architecture behavioral of biport_tb is
   component biport is
     port (
           send : in std_logic_vector(3 downto 0);
